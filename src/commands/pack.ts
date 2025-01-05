23d2e0b55f5c189366be59e9d6b8fcf061559c24
Lucas <lucasnrgaard@gmail.com>
2025-01-05 18:18:28 +0100

    chore: dump

--- src/commands/pack.ts
+++ src/commands/pack.ts
@@ -1,7 +1,7 @@
 import type { PackageManager } from "vsix-utils";
 import process from "node:process";
 import { defineCommand } from "citty";
-import { green, red, underline } from "farver";
+import { green, red, underline, yellow } from "farver/fast";
 import { createVsix } from "vsix-builder";
 
 export default defineCommand({
@@ -10,54 +10,76 @@ export default defineCommand({
     description: "Pack Visual Studio Code Extensions",
   },
   args: {
-    cwd: {
+    "cwd": {
       type: "string",
       description: "Working directory",
       required: false,
     },
-    skipScripts: {
+    "skip-scripts": {
       type: "boolean",
       description: "Skip running vscode scripts",
       required: false,
       default: false,
     },
-    packagePath: {
+    "package-path": {
       type: "string",
       description: "The destination path for the VSIX package",
       required: false,
     },
-    packageManager: {
+    "package-manager": {
       type: "string",
       description: "Package Manager",
       required: false,
       default: "auto",
       alias: "pm",
     },
-    dryRun: {
+    "dry-run": {
       type: "boolean",
       description: "Dry run",
       required: false,
       default: false,
     },
-    readme: {
+    "readme": {
       type: "string",
       description: "Path to README file",
       required: false,
     },
-    dependencies: {
+    "dependencies": {
       type: "boolean",
       description: "Include dependencies in the package, only useful it you are not bundling the extension",
       required: false,
       default: true,
     },
+    "force": {
+      type: "boolean",
+      description: "Force write to disk",
+      required: false,
+      default: false,
+    },
+    "pre-release": {
+      type: "boolean",
+      description: "Pre-release",
+      required: false,
+      default: false,
+    },
+    "ignore-file": {
+      type: "string",
+      description: "Ignore file",
+      required: false,
+    },
   },
   async run({ args }) {
     const cwd = args.cwd ?? process.cwd();
     const vsix = await createVsix({
-      write: !args.dryRun,
+      write: !args["dry-run"],
+      forceWrite: args.force,
       cwd,
-      packagePath: args.packagePath,
-      packageManager: args.pm as PackageManager,
+      packagePath: args["package-path"],
+      packageManager: args["package-manager"] as PackageManager,
+      readme: args.readme,
+      preRelease: args["pre-release"],
+      ignoreFile: args["ignore-file"],
+      skipScripts: args["skip-scripts"],
     });
 
     if (vsix.errors.length > 0) {
@@ -67,6 +89,11 @@ export default defineCommand({
         console.error(` - ${red(error.type)}${"message" in error ? `: ${error.message}` : ""}`);
       }
 
+      return;
+    }
+
+    if (!vsix.written) {
+      console.warn(`no vsix package was created, as you are in ${yellow("dry-run")} mode`);
       return;
     }
 
