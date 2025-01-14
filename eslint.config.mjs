340abef939804e27ffd1f1512b3bce0de222dcdb
Lucas <lucasnrgaard@gmail.com>
2025-01-14 07:12:47 +0100

    chore: update ecma version in eslint By default my personal config is using 2022, which doesn't have the import attributes. Therefore ESLint fails on syntax parsing.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,10 @@ import luxass from "@luxass/eslint-config";
 export default luxass({
   formatters: true,
   type: "lib",
+  languageOptions: {
+    ecmaVersion: "latest",
+    parserOptions: {
+      ecmaVersion: "latest",
+    },
+  },
 });
